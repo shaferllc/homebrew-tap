fd082d4d397b5de6d9835f8800706f477b73f75f
Tom Shafer <tj@tjshafer.com>
2026-09-13 12:30:23 -0700

    Drop the deprecated verified: from Ledge's url

--- Casks/ledge.rb
+++ Casks/ledge.rb
@@ -2,8 +2,7 @@ cask "ledge" do
   version "1.1"
   sha256 "43e675d969aac6decc544949085e27504d1fc07d39ea80d8491596d27ddd7dd2"
 
-  url "https://dl.shafer.llc/ledge/Ledge-#{version}.dmg",
-      verified: "dl.shafer.llc/ledge/"
+  url "https://dl.shafer.llc/ledge/Ledge-#{version}.dmg"
   name "Ledge"
   desc "Turns the MacBook notch into a hover-expand dashboard"
   homepage "https://ledge.shafer.llc/"
